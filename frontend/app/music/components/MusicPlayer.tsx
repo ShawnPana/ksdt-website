ff2590d04c65c324b88f9fadcd1884fb52cecc05
warren <warren>
2025-11-21 20:10:35 -0800

    fix chrome misalignment issue

--- frontend/app/music/components/MusicPlayer.tsx
+++ frontend/app/music/components/MusicPlayer.tsx
@@ -3,13 +3,13 @@
 export default function MusicPlayer() {
 
   return (
-    <div className="overflow-hidden relative">
+    <div className="overflow-hidden relative pb-4">
       <div className="absolute top-[1px] left-[5px] text-white">
         <a href="https://s4.radio.co/s2c33c7adb/listen">RADIO.CO STREAM</a>
       </div>
       <iframe
         src="https://embed.radio.co/player/d70721f.html"
-        className="w-98 h-102 border-none block overflow-hidden white"
+        className="w-[391px] h-[391px] border-none block overflow-hidden white"
         title="KSDT Music Radio.co Player"
         scrolling="no"
       />
--- frontend/app/music/components/NowPlaying.tsx
+++ frontend/app/music/components/NowPlaying.tsx
@@ -3,10 +3,10 @@
 export default function NowPlaying() {
 
   return (
-    <div className="overflow-hidden">
+    <div className="flex flex-start overflow-hidden pb-4">
       <iframe
-        src="https://lastfm-recently-played.vercel.app/api?user=KSDT-RADIO&nowplaying=true&count=6&show_user=header"
-        className="w-100 h-102"
+        src="https://lastfm-recently-played.vercel.app/api?user=KSDT-RADIO&nowplaying=true&count=6&show_user=header&width=391"
+        className="w-98 h-98 top-0 block align-top"
         title="KSDT Radio Last.fm"
         scrolling="no"
       />
