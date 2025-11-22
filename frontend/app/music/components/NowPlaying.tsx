b32dcd4c964818e8f40c430b8d4dd9903cff1541
warren <warren>
2025-11-21 23:22:26 -0800

    remove useless tag

--- frontend/app/music/components/NowPlaying.tsx
+++ frontend/app/music/components/NowPlaying.tsx
@@ -6,7 +6,7 @@ export default function NowPlaying() {
     <div className="overflow-hidden">
       <iframe
         src="https://lastfm-recently-played.vercel.app/api?user=KSDT-RADIO&nowplaying=true&count=5&show_user=header&width=334"
-        className="w-[334px] h-[334px] top-0"
+        className="w-[334px] h-[334px]"
         title="KSDT Radio Last.fm"
         scrolling="no"
       />
